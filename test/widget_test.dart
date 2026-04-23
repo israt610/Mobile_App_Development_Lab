8b1bffa42112e51cdd38ebaba0b9c5c75d89d128
israt610 <israt.cse9.bu@gmail.com>
2026-04-23 22:48:02 +0600

    Added from practice 01

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:flutter_mad_lab/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(BUCSE9());
+    await tester.pumpWidget(MainApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
